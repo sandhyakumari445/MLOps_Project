7ab1c1d662df793675f63b114621fcf90f4bbf36
Sandhya <sandhyakumari445.sk@gmail.com>
2023-04-13 11:30:52 +0530

    Model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,8 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -41,5 +43,11 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data=obj.initiate_data_ingestion()
+
+    data_transformation= DataTransformation()
+    train_arr, test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
     
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+ 
---
+++ src/components/model_trainer.py
@@ -0,0 +1,97 @@
+import os
+import sys
+from dataclasses import dataclass
+#from src.logger import logging
+
+from sklearn.ensemble import (
+    AdaBoostRegressor, 
+    GradientBoostingRegressor,
+    RandomForestRegressor
+    
+)
+from catboost import CatBoostRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRFRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object, evaluate_models
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config= ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info(f"Spliting training and test input data")
+            X_train, y_train, X_test, y_test= (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+
+            )
+
+            models={
+                "Random_Forest":RandomForestRegressor(),
+                "Decision_Tree": DecisionTreeRegressor(),
+                "Gradient_Boosting": GradientBoostingRegressor(),
+                "Linear_Regression": LinearRegression(),
+                "K-Neighbour_Regressor": KNeighborsRegressor(),
+                "XGBRegressor": XGBRFRegressor(),
+                "Catboosting_Regressor": CatBoostRegressor(),
+                "Adaboost_Regressor": AdaBoostRegressor()
+
+            }   
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                              models=models)
+
+            best_model_score= max(sorted(model_report.values()))
+
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model=models[best_model_name]
+
+
+        
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best model found on training and testing datasets")
+            
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted=best_model.predict(X_test)
+            r2_square=r2_score(y_test, predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
